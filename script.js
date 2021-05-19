81a601de0578b033bf30579f4ad2c66b4e4fe70d
John Alon <65182377+JohnAlon@users.noreply.github.com>
2021-05-19 20:23:35 +0300

    V0.13

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
 let entity = document.getElementById('entity')
 let entityHp = document.getElementById('entityHp')
+let killDispaly = document.getElementById('kills')
 let hp = 1000
+let kills = 0
 entity.addEventListener('click', function(){
     hp--
     entityHp.textContent = 'HP: ' + hp
 
-    if (hp == 0){
+    if (hp <= 0){
+
         hp = hp + 1000
+        kills++
+        killDispaly.textContent = 'Kills: ' + kills
     }
 
     function progress() {
