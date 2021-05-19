35b5e23d74e86e505a49f21bd74f5976e4b43052
John Alon <65182377+JohnAlon@users.noreply.github.com>
2021-05-19 13:58:58 +0300

    V 0.12

--- script.js
+++ script.js
@@ -1,10 +1,19 @@
 let entity = document.getElementById('entity')
 let entityHp = document.getElementById('entityHp')
-let hp = 500
+let hp = 1000
 entity.addEventListener('click', function(){
     hp--
     entityHp.textContent = 'HP: ' + hp
+
     if (hp == 0){
-        hp = hp +1500
+        hp = hp + 1000
     }
+
+    function progress() {
+        let elem = document.getElementById('progressLine')
+        let per = hp / 10
+        elem.style.width = per + '%';
+    }
+    progress();
+
 });
