e63463b435d3fb57cb9c7be7cfcc8b15bf43b920
John Alon <65182377+JohnAlon@users.noreply.github.com>
2021-05-20 10:33:21 +0300

    V 0.14

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ let entity = document.getElementById('entity')
 let entityHp = document.getElementById('entityHp')
 let killDispaly = document.getElementById('kills')
 let damageDisplay = document.getElementById('damage')
-let hp = 10
+let hp = 1000
 let kills = 0
 let bonus = 0
 entity.addEventListener('click', function(){
@@ -13,7 +13,7 @@ entity.addEventListener('click', function(){
 
     if (hp <= 0){
 
-        hp = 10
+        hp = 1000
         kills++
         killDispaly.textContent = 'Kills: ' + kills
 
