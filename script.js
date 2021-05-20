c8e20126d9eb94be8ec86ae441a8d65fdb234fe2
John Alon <65182377+JohnAlon@users.noreply.github.com>
2021-05-20 10:30:38 +0300

    V 0.14

--- script.js
+++ script.js
@@ -1,17 +1,29 @@
 let entity = document.getElementById('entity')
 let entityHp = document.getElementById('entityHp')
 let killDispaly = document.getElementById('kills')
-let hp = 1000
+let damageDisplay = document.getElementById('damage')
+let hp = 10
 let kills = 0
+let bonus = 0
 entity.addEventListener('click', function(){
-    hp--
+    let damage = 1 + bonus
+    hp = hp - damage
     entityHp.textContent = 'HP: ' + hp
+    let killBonus = kills % 10
 
     if (hp <= 0){
 
-        hp = hp + 1000
+        hp = 10
         kills++
         killDispaly.textContent = 'Kills: ' + kills
+
+        if (kills >= 10) {
+            let x = kills % 10
+            if (x == 0) {
+                bonus ++
+            }
+        }
+        damageDisplay.textContent = 'Damage: ' + (bonus + 1)
     }
 
     function progress() {
@@ -20,5 +32,7 @@ entity.addEventListener('click', function(){
         elem.style.width = per + '%';
     }
     progress();
+
+    console.log(kills + ' --- ' + bonus)
 
 });
